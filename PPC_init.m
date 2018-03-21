e22f7c4b85266715013b2be0173720811bdeae83
James E. Carmichael <f00287r@discovery.hpcc.dartmouth.edu>
2018-03-20 22:22:23 -0400

    initilization for Discovery

--- PPC_init.m
+++ PPC_init.m
@@ -13,6 +13,7 @@ cfg_in.data_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R111-2017-06-20-Rec_auto for Er
 cfg_in.inter_dir = '/dartfs-hpc/rc/lab/M/MeerM/EC/R111-2017-06-20-Rec_auto for Eroc'; 
 cfg_in.phase = 1; % corresponds to the first recording phase ('pre').  2 = 'task', 3 = 'post'.
 cfg_in.shuffle = 100; 
+cfg_in.plot = 1;
 cfg_in.min_nSpikes = 500; 
 
 
